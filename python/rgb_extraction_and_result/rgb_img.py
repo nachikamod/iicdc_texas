e61abc630061976ed59ff1b062bfe7bc81736818
Nachiket Kamod <47713768+nachikamod@users.noreply.github.com>
2020-02-26 15:51:20 +0530

    RGB Extraction algorithm first commit
    RGB Extraction algorithm for color extraction from plant leaves. First, commit can understand yellow color from center 3*3 RGB matrix and conditional statement states if the image's center contains yellow color or not. The basic matrix algorithm is implemented to retrieve the mean value of RGB color at the center of the image. 
    PIL library is used for RGB extraction.
    While using this code make sure to change the directory of the images to your specified location.

---
+++ python/rgb_extraction_and_result/rgb_img.py
@@ -0,0 +1,58 @@
+import os 
+import time
+from PIL import Image
+import cv2 
+
+dir = "D:\\workspace\\python\\vscode\\file_opener\\test_dir"
+
+files = os.listdir(dir)
+
+for x in files:
+    rot = [0,0,0]
+    im = Image.open(dir + "\\" + x)
+    pix = im.load()
+    im_size = im.size
+    
+    im_height = im_size[0]
+    im_width = im_size[1]
+
+    print(x)
+    print( )
+
+    for i in range(-1,2):
+        for j in range(-1,2):
+            
+            tup = pix[(im_height/2)+i,(im_width/2)+j]
+
+            for k in range(3):
+                rot[k] += tup[k]
+                
+                if i == 1 and j == 1 and k == 2:
+                    rgb_dump = [number / 9 for number in rot]
+                    print(rgb_dump)
+
+                    if int(rgb_dump[0]) in range(150,256) and int(rgb_dump[1]) in range(150,256) and int(rgb_dump[2]) in range(-1,56):
+                        print("Yellow found")
+
+    #u_pix1 = pix[(im_height/2)-1,(im_width/2)+1]
+    #print(u_pix1)
+    #u_pix2 = pix[(im_height/2)-1,(im_width)/2]
+    #u_pix3 = pix[(im_height/2)-1,(im_width/2)-1]
+
+    #m_pix1 = pix[(im_height/2),(im_width/2)+1]
+    #m_pix2 = pix[(im_height/2),(im_width)/2]
+    #m_pix3 = pix[(im_height/2),(im_width/2)-1]
+
+    #l_pix1 = pix[(im_height/2)+1,(im_width/2)+1]
+    #l_pix2 = pix[(im_height/2)+1,(im_width/2)]
+    #l_pix3 = pix[(im_height/2)+1,(im_width/2)-1]
+
+    #pixMean_1 = u_pix1[0] + u_pix2[0] + u_pix3[0] + m_pix1[0] + m_pix2[0] + m_pix3[0] + l_pix1[0] + l_pix2[0] + l_pix3[0]
+    #pixMean_2 = u_pix1[1] + u_pix2[1] + u_pix3[1] + m_pix1[1] + m_pix2[1] + m_pix3[1] + l_pix1[1] + l_pix2[1] + l_pix3[1]
+    #pixMean_3 = u_pix1[2] + u_pix2[2] + u_pix3[2] + m_pix1[2] + m_pix2[2] + m_pix3[2] + l_pix1[2] + l_pix2[2] + l_pix3[2]
+    #print(pixMean_1/9)
+    #print(pixMean_2/9)
+    #print(pixMean_3/9)
+    #print( )
+
+    
