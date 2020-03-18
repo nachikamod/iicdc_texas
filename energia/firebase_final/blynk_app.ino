7d46941eb2714e39318f61e54a29120a1ccbf99e
Nachiket Kamod <47713768+nachikamod@users.noreply.github.com>
2020-03-18 18:01:35 +0530

    Comments added

--- energia/firebase_final/blynk_app.ino
+++ energia/firebase_final/blynk_app.ino
@@ -1,48 +1,24 @@
-/**************************************************************
- * Blynk is a platform with iOS and Android apps to control
- * Arduino, Raspberry Pi and the likes over the Internet.
- * You can easily build graphic interfaces for all your
- * projects by simply dragging and dropping widgets.
- *
- *   Downloads, docs, tutorials: http://www.blynk.cc
- *   Blynk community:            http://community.blynk.cc
- *   Social networks:            http://www.fb.com/blynkapp
- *                               http://twitter.com/blynk_app
- *
- * Blynk library is licensed under MIT license
- * This example code is in public domain.
- *
- **************************************************************
- * This example shows how to use TI CC3200-LaunchXL
- * to connect your project to Blynk.
- *
- * Feel free to apply it to any other example. It's simple!
- *
- **************************************************************/
-
-#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#define BLYNK_PRINT Serial
 #include <SPI.h>
 #include <WiFi.h>
 #include <BlynkSimpleTI_CC3200_LaunchXL.h>
 #include <Wire.h>
 
-int timer_counter = 0;
-boolean enable_count = false; 
+int timer_counter = 0;    //timer for storing for how much time an button is clicked
+boolean enable_count = false;   //conditional boolean to enable and disssable counter
 
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
-char auth[] = "KkhXYm9SEnhXZuRMIUeXJbqSApDM_3jZ";
+char auth[] = "KkhXYm9SEnhXZuRMIUeXJbqSApDM_3jZ"; //Auth token form blynk app
 
 // Your WiFi credentials
-char ssid[] = "Soham";
-char pass[] = "Nachidk99999*";        // Set to "" for open networks
+char ssid[] = "Wifi name here";
+char pass[] = "Password here"; 
 int dataX = 0;
 int dataY = 0;
 int dataZ = 0;
 
-int commands[1000];
-int delay_commands[1000];
-int comm_count = 0;
+int commands[1000];     //array to store commands
+int delay_commands[1000];   //array to store commands execution timings
+int comm_count = 0;         //number of commands counter
 
 void setup()
 {
@@ -78,17 +54,18 @@ BLYNK_WRITE(V1)
   {
     digitalWrite(RED_LED, LOW);
 
-    // 1 is conditional command for red led for automation
-    commands[comm_count] = 1;
-    delay_commands[comm_count] = timer_counter;
-
-    comm_count += 1;
     
-    timer_counter = 0;
-    enable_count = false;
+    commands[comm_count] = 1;   // 1 is conditional command for red led for automation
+    delay_commands[comm_count] = timer_counter;     //get time of click from timer
+
+    comm_count += 1;           // after updating command and delay in array append the index of array to save next command in next index
+    
+    timer_counter = 0;         //after storing the command clear the counter
+    enable_count = false;      //turn off the counter
   }
 }
 
+//for yellow led
 BLYNK_WRITE(V2) {
   BLYNK_LOG("Got a value to Yellow: %s", param.asStr());
   
@@ -113,6 +90,7 @@ BLYNK_WRITE(V2) {
   }
 }
 
+//for green led
 BLYNK_WRITE(V3) {
   BLYNK_LOG("Got a value to GREEN: %s", param.asStr());
   
@@ -137,23 +115,34 @@ BLYNK_WRITE(V3) {
   }
 }
 
+//for automation led
 BLYNK_WRITE(V4) {
 
   BLYNK_LOG("Got a value to AUTO: %s", param.asStr());
   int i = param.asInt(); 
   if(i == 1)
   {     
-       int nxtComm = 0;
-       while(commands[nxtComm] != 0) {
-          switch(commands[nxtComm]) {
+       int nxtComm = 0;   //command index to read from array
+       while(commands[nxtComm] != 0)
+       /*
+       check if the command is empty at specific index, if we dont use this step then our
+       any loop will have to iterate through whole array that is 1000 count.
+        Hence by using this method we check if there is any command at current index if yes then execute forward else break
+       */
+       
+       {   
+         
+         switch(commands[nxtComm]) { //Getting commands from array we previously stored
             case 1:digitalWrite(RED_LED,HIGH);
                    
+                    //generating delay using for loop as you can see we also stored the time of button click and hold
                    for(int m = 0; m < delay_commands[nxtComm]; m++){
                     
                     Serial.println(m);
                     
                    }
                    
+             //turning off the led after the delay ends
                    digitalWrite(RED_LED,LOW);
                    break;
             case 2:digitalWrite(YELLOW_LED,HIGH);
@@ -171,13 +160,16 @@ BLYNK_WRITE(V4) {
                    digitalWrite(GREEN_LED,LOW);
                    break;  
           }
-          nxtComm += 1;
+          nxtComm += 1; // after executing first case that is oth index of array increase the nxtComm by one to read next index for next command and delay 
        }
+    
+      //after successfull execution of all commands in array reset the index to zero again
        nxtComm = 0;
        
   }
   else if(i == 0)
   {
+    //if automation button is turned off clear the commands and delay array
     int errase = 0;
     comm_count = 0;
     while(commands[errase] != 0) {
@@ -195,6 +187,8 @@ BLYNK_WRITE(V4) {
 void loop()
 {
   Blynk.run();
+  
+  //counter to count button click time
   if(enable_count == true) {
     timer_counter += 1; 
     Serial.println(timer_counter);
