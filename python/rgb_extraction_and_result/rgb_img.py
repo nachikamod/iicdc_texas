799defd1e7aef361432c6dbe8d971162919731fb
Nachiket Kamod <47713768+nachikamod@users.noreply.github.com>
2020-03-18 18:05:11 +0530

    redundant comments removed

--- python/rgb_extraction_and_result/rgb_img.py
+++ python/rgb_extraction_and_result/rgb_img.py
@@ -34,29 +34,3 @@ for x in files:
                     if int(rgb_dump[0]) in range(150,256) and int(rgb_dump[1]) in range(150,256) and int(rgb_dump[2]) in range(-1,56):
                         print("Yellow found")
 
-                        
-                        
-    #Manual rgb ret and calc
-    
-    #u_pix1 = pix[(im_height/2)-1,(im_width/2)+1]
-    #print(u_pix1)
-    #u_pix2 = pix[(im_height/2)-1,(im_width)/2]
-    #u_pix3 = pix[(im_height/2)-1,(im_width/2)-1]
-
-    #m_pix1 = pix[(im_height/2),(im_width/2)+1]
-    #m_pix2 = pix[(im_height/2),(im_width)/2]
-    #m_pix3 = pix[(im_height/2),(im_width/2)-1]
-
-    #l_pix1 = pix[(im_height/2)+1,(im_width/2)+1]
-    #l_pix2 = pix[(im_height/2)+1,(im_width/2)]
-    #l_pix3 = pix[(im_height/2)+1,(im_width/2)-1]
-
-    #pixMean_1 = u_pix1[0] + u_pix2[0] + u_pix3[0] + m_pix1[0] + m_pix2[0] + m_pix3[0] + l_pix1[0] + l_pix2[0] + l_pix3[0]
-    #pixMean_2 = u_pix1[1] + u_pix2[1] + u_pix3[1] + m_pix1[1] + m_pix2[1] + m_pix3[1] + l_pix1[1] + l_pix2[1] + l_pix3[1]
-    #pixMean_3 = u_pix1[2] + u_pix2[2] + u_pix3[2] + m_pix1[2] + m_pix2[2] + m_pix3[2] + l_pix1[2] + l_pix2[2] + l_pix3[2]
-    #print(pixMean_1/9)
-    #print(pixMean_2/9)
-    #print(pixMean_3/9)
-    #print( )
-
-    
