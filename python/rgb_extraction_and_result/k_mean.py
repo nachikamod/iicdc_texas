678914c66b2fb8a865eb98cacdee03d29ddf42cf
N V Roshni <roshstar1999@gmail.com>
2020-03-19 14:04:39 +0530

    Update k_mean.py

--- python/rgb_extraction_and_result/k_mean.py
+++ python/rgb_extraction_and_result/k_mean.py
@@ -11,12 +11,12 @@ files = os.listdir(dir)
 for x in files:
 
     img = cv2.imread('D:/workspace/python/vscode/file_opener/test_dir/' + x)
-    img2 = img.reshape((-1,3))
-    img2 = np.float32(img2)
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER , 10, 1.0)
+    img2 = img.reshape((-1,3))                                                  #to flatten the image,from pixel cordinate format to pixel area format 
+    img2 = np.float32(img2)                                                     #for opencv opns we need float32 datatype(by def its int8)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER , 10, 1.0)   #criteria for stopping the calcs 
 
-    k = 5
-    attempts = 10
+    k = 5                                                                     #no. of clusters to be formed                                                               
+    attempts = 10                                                              
     ret, label, center = cv2.kmeans(img2,k,None, criteria, attempts, cv2.KMEANS_PP_CENTERS)
 
     center = np.uint8(center)
