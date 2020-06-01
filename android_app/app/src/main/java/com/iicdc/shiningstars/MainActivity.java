8c442d61f4241357991d8fe44bcf1f8258af9ab0
Nachiket Kamod <47713768+nachikamod@users.noreply.github.com>
2020-06-01 12:23:30 +0530

    Android Commit 1: Splash screen and login screen activity created

--- android_app/app/src/main/java/com/iicdc/shiningstars/MainActivity.java
+++ android_app/app/src/main/java/com/iicdc/shiningstars/MainActivity.java
@@ -2,13 +2,22 @@ package com.iicdc.shiningstars;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent loginIntent = new Intent();
+        startActivity(loginIntent);
+        finish();
     }
 }
---
+++ android_app/app/src/main/java/com/iicdc/shiningstars/loginScreen.java
@@ -0,0 +1,16 @@
+package com.iicdc.shiningstars;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class loginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+
+
+    }
+}
